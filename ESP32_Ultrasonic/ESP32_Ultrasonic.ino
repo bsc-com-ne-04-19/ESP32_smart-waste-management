03dc558c007a09513672a05fb763c6d1d0b9f5cf
MPHATSO MATEMBA <BSC/COM/NE/04/19@COMLAB.COM>
2024-07-11 16:13:12 +0200

    sending ultrasonic sensor readings to cloud platform(thingspeak)

--- ESP32_Ultrasonic/ESP32_Ultrasonic.ino
+++ ESP32_Ultrasonic/ESP32_Ultrasonic.ino
@@ -1,7 +1,7 @@
-#include <Wifi.h>
+#include <WiFi.h>
 #include <HTTPClient.h>
 #include <Wire.h>
-#include <Conf.h>   // configuration file for wifi and thingspeak credentials
+#include "Conf.h"  // configuration file for wifi and thingspeak credentials
 
 #define echoPin 2
 #define trigPin 4
@@ -17,15 +17,15 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
-  Wifi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while(Wifi.status() != WL_CONNECTED){
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while(WiFi.status() != WL_CONNECTED){
     delay(500);
     Serial.print(".");
   }
-  Serial.Println("");
+  Serial.println("");
   Serial.println("Wifi connected");
   Serial.println("IP address: ");
-  Serial.println(Wifi.localIP());
+  Serial.println(WiFi.localIP());
 
 }
 
@@ -34,7 +34,7 @@ void loop() {
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW):
+  digitalWrite(trigPin, LOW);
 
   duration = pulseIn(echoPin, HIGH);
   distance = duration * 0.034 / 2;
@@ -46,9 +46,30 @@ void loop() {
   //calculate bin as percentage in relation to distance
   fillLevel = 100 - ((distance * 100) / Max_DISTANCE);
   if (fillLevel < 0) fillLevel = 0;
-  if (fillLevel > 100) fillLevel = 100
+  if (fillLevel > 100) fillLevel = 100;
   Serial.print("Fill Level: ");
   Serial.print(fillLevel);
   Serial.println("%");
+
+  if (WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+    String url = String(SERVER) + "?api_key=" + WRITE_APIKEY +  "$field1=" + String(fillLevel);
+    http.begin(url);
+    int httpResponseCode = http.GET();
+    if (httpResponseCode > 0){
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+
+    }else{
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+
+    }
+    http.end();
+  }else{
+    Serial.println("Wifi Disconnected");
+  
+  }
+  delay(15000); // send data every 15 seconds 
 
 }
